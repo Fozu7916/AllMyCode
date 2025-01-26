2630f19f5ddd05776c518981780fb45b3c4469d7
AleshaFozu <132448722+Fozu7916@users.noreply.github.com>
2025-01-26 18:27:25 +0700

    Update hel12p.cpp

--- C++/Console/hel12p.cpp
+++ C++/Console/hel12p.cpp
@@ -3,12 +3,16 @@
 #include <string>
 using namespace std;
 
-
+// класс хеширования пароля
 class HeshingPassword {
 public:
 	vector<char> baze;
+
+// сам принцип хеширования таков: каждый символ  превращается в {asci номер символа + 12}
 	const int incoming = 12;
 
+
+// получить наш пароль
 	string GetPas()
 	{
 		string result = "";
@@ -18,7 +22,7 @@ public:
 		}
 		return result;
 	}
-
+//записываем наш пароль из сроки в массив чаров
 	void SetPas(string here)
 	{
 		baze.resize(here.length());
@@ -27,7 +31,7 @@ public:
 			baze[i] = here[i];
 	    }
 	}
-
+//сам процесс хеширования
 	void HashPas(string here)
 	{
 		for (int i = 0; i < baze.size(); i++)
